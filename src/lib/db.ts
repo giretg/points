63867084526099a8cf8c9cd90bbaa202e1bbc8bc
giretg <84626702+giretg@users.noreply.github.com>
2024-12-17 14:13:55 +0100

    jav2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,23 +1,13 @@
-import { MongoClient } from 'mongodb'
+import mongoose from "mongoose";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('Please add your MongoDB URI to .env.local')
+export const connectMongoDb= async ()=>{
+    console.log("kapcsolat meghívva!")
+    try{
+        await mongoose.connect(process.env.DATABASE_URL!);
+        console.log('Connected to mongoDB, perfect:');
+    }catch(e){
+        console.log('Error connecting mongoDB:', e);
+    }
+
+
 }
-
-let client: MongoClient
-let clientPromise: Promise<MongoClient>
-
-if (process.env.NODE_ENV === 'development') {
-  // Development módban újrahasználjuk a kapcsolatot
-  if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(process.env.DATABASE_URL)
-    ;(global as any)._mongoClientPromise = client.connect()
-  }
-  clientPromise = (global as any)._mongoClientPromise
-} else {
-  // Production módban új kapcsolatot hozunk létre
-  client = new MongoClient(process.env.DATABASE_URL)
-  clientPromise = client.connect()
-}
-
-export default clientPromise
